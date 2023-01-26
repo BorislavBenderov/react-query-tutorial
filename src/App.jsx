39fe673456b5f5b857c1dc6e7abe1dad367e7042
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-26 09:20:53 +0200

    Add basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import { TodoList } from "./features/todos/TodoList";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Profile } from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+      </Routes>
     </div>
   );
 }
