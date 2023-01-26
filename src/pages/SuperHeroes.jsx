1dc103c52686910fd59a497d292b6e9feb19badf
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-26 12:25:18 +0200

    Fetch data with useQuery and render heroes names

--- src/pages/SuperHeroes.jsx
+++ src/pages/SuperHeroes.jsx
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export const SuperHeroes = () => {
+  const { data, isLoading } = useQuery(["super-heroes"], () => {
+    return axios
+      .get("http://localhost:3500/superheroes")
+      .then((res) => res.data);
+  });
+  console.log(data);
   return (
-    <div>SuperHeroes</div>
-  )
-}
+    <div>
+      {isLoading
+        ? "Loading..."
+        : data.map((hero) => <div key={hero.name}>{hero.name}</div>)}
+    </div>
+  );
+};
