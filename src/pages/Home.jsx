83fd5c805b497d5f4dbeb16a5eb7fff053b0986d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-26 09:50:18 +0200

    Fetch data with react query and use refetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export const Home = () => {
+  const { data, isLoading, isError, refetch } = useQuery(["cat"], () => {
+    return axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Home</h1>
+      <p>{isLoading ? "Loading..." : data?.fact}</p>
+      <button onClick={refetch}>Update</button>
+    </div>
+  );
+};
