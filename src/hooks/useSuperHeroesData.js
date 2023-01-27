323ce735911a863c8c21b069b7b61bd0442818c3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-27 10:50:58 +0200

    Add useAddSuperHeroData hook

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,5 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from "axios";
+
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:3500/superheroes", hero);
+};
 
 export const useSuperHeroesData = (onSuccess, onError) => {
   return useQuery(
@@ -14,4 +18,8 @@ export const useSuperHeroesData = (onSuccess, onError) => {
       onError,
     }
   );
+};
+
+export const useAddSuperHeroData = () => {
+  return useMutation(addSuperHero);
 };
